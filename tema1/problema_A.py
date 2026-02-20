934992f6956d639bd3b66ab6a1256178fc9c6b28
Lucía <lusanmanso@gmail.com>
2026-02-20 14:18:05 +0100

    T1: Corregido A

--- tema1/problema_A.py
+++ tema1/problema_A.py
@@ -10,8 +10,15 @@ def roots(a, b, c):
    x2 = (-b - sd) / (2*a)
    return x1, x2
 
-a, b, c = 1, 9**12, -3
+# a, b, c = 1, 9**12, -3
+a, b, c = 1, 10**8, 1
 
 x1_cc, x2 = roots(a, b, c)
 print(f"Raíz x1 con cancelación catastrófica: {x1_cc}")
 print(f"Raíz x2 con cancelación catastrófica: {x2}")
+
+x1 = c / (x2 * a)
+print (f"Raíz x1 sin cancelación catastrófica: {x1:e}")
+
+print(f"Error absoluto: {abs(x1 - x1_cc):e}")
+print(f"Error relativo: {abs(x1 - x1_cc) / abs(x1):e}")
